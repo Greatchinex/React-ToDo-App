d8e1edd41be8035f6286e8dfbdc26c8ccceb83a4
Chilaka Chinedu <greatchinex@gmail.com>
2019-05-28 19:50:22 +0100

    Displayed Todos with Basic Styling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,  { Component } from 'react';
+import Todos from './Components/Todos';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       todos: [
+         {
+           id: 1,
+           title: "Learn React",
+           completed: false
+         },
+         {
+          id: 2,
+          title: "Make a Todo App",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Get a Developer Job",
+          completed: false
+        }
+       ]
+    }
+  }
+    
+
+  render() {
+      return (
+        <div className="App">
+          <Todos todo={this.state.todos} />
+        </div>
+      )
+  }
 }
+
+
+
+
+
+
+
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <Todos />
+//     </div>
+//   );
+// }
 
 export default App;
