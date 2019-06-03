db481e32c1cb22a38f38f102fd6dd217f7c744e0
Chilaka Chinedu <greatchinex@gmail.com>
2019-06-03 19:03:41 +0100

    Updates

---
+++ precache-manifest.203b547bb0481e4fefb2cdd5c12aaf2c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2021390529924622eadd6c5dfa298846",
+    "url": "/React-ToDo-App/index.html"
+  },
+  {
+    "revision": "210ebb3c7d05ec0fbe3d",
+    "url": "/React-ToDo-App/static/css/main.ba44d2b8.chunk.css"
+  },
+  {
+    "revision": "d4aa56fd55d16204d267",
+    "url": "/React-ToDo-App/static/js/2.d893684d.chunk.js"
+  },
+  {
+    "revision": "210ebb3c7d05ec0fbe3d",
+    "url": "/React-ToDo-App/static/js/main.df9015a0.chunk.js"
+  },
+  {
+    "revision": "f1d8c583e886a0a21978",
+    "url": "/React-ToDo-App/static/js/runtime~main.faef495b.js"
+  }
+]);
